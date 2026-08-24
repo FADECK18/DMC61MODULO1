21e307828b84d09ff3ec53819c3bdf7cddf1d0ab
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 12:08:36 -0500

    Reorganize input handling in Streamlit app

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
-valor_final = st.number_input("Ingrese el valor final")
-
-lista_numeros = list(range(int(valor_inicial), int(valor_final)))
-st.write(lista_numeros)
-
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
 
 if modulos == "Módulo Listas":
+    
     st.write("Bienvenido al módulo Listas")
+    valor_inicial = st.number_input("Ingrese el valor inicial")
+    valor_final = st.number_input("Ingrese el valor final")
+    
+    lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+    st.write(lista_numeros)
+
 elif modulos == "Módulo Arreglos":
+    
     st.write("Bienvenido al módulo de Arreglos")
+    
 else:
+    
     st.write("Bienvenido al módulo de Funciones")                                                    

a54de7daf677f64f176dc259f5e1b917abd5594a
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 11:34:55 -0500

    Generate list of numbers from input values
    Added functionality to generate a list of numbers based on user input.

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parametros")
-st.write("Elaborado por: Farid Garibay Fabian")
+st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
 valor_inicial: st.number_input("Ingrese el valor inicial")
 valor_final: st.number_input("Ingrese el valor final")
 
+lista_números = list(range(valor_inicial, valor_final))
+
+st.write(lista_números)

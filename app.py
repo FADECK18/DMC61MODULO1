c9dd0875c76921f8e784532766f6dc433c79ae12
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 11:30:51 -0500

    Add number input fields for initial and final values
    Added input fields for initial and final values.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Farid Garibay Fabian")
+
+valor_inicial: st.number_input("Ingrese el valor inicial")
+valor_final: st.number_input("Ingrese el valor final")
